a0982befe29ed868b503d39cd94fdf372818c671
Peter McCallum <85507238+pmccallu@users.noreply.github.com>
2022-03-16 13:56:33 -0600

    env bug

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -8,8 +8,7 @@ export default function Home() {
       header="BadBank Landing Module"
       title="Peter Demo Bank"
       text="Welcome to the Peter Demo Bank.  You can move around using the navigation bar. 
-      This is a single page application built with React to simulate an ATM application. Each account starts with $100,
-      afterwards you can deposit or withdraw money or check the balance page."
+      This is a single page application built with React to simulate an ATM application. There is a MongoDB database and it was hosted with Heroku."
       body={<img src="/bank.jpeg" className="img-fluid" alt="Responsive" />}
     />
   );
