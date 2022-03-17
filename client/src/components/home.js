cd5495b8be30949fc35ee062fc68164abf66f479
Peter McCallum <85507238+pmccallu@users.noreply.github.com>
2022-03-17 09:52:30 -0600

    removed space in procfil

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -8,7 +8,7 @@ export default function Home() {
       header="BadBank Landing Module"
       title="Peter Demo Bank"
       text="Welcome to the Peter Demo Bank Capstone project.  You can move around using the navigation bar. 
-      This is a single page application built with React to simulate an ATM application. 
+      This is a single page application built with React to simulate an ATM application. There are deposit, withdraw and transfer features.
       There is a MongoDB database and it was hosted with Heroku."
       body={<img src="/bank.jpeg" className="img-fluid" alt="Responsive" />}
     />
