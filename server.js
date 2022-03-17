19eb81f4a4ad28e90cdf5650286add782bfcdae7
Peter McCallum <85507238+pmccallu@users.noreply.github.com>
2022-03-17 09:58:21 -0600

    added port var in serverjs

--- server.js
+++ server.js
@@ -265,7 +265,7 @@ app.post("/api/user/balance/transfer", async (req, res) => {
 // require("./routes/user.routes")(app);
 
 // set port, listen for requests
-//const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
