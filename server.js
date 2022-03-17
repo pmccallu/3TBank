91466789c8ad7b5f10a7ac320430085699faf515
Peter McCallum <85507238+pmccallu@users.noreply.github.com>
2022-03-17 15:36:20 -0600

    corsoptions commented out

--- server.js
+++ server.js
@@ -35,15 +35,15 @@ db.mongoose
     process.exit();
   });
 
-var corsOptions = {
-  origin: "http://localhost:3000",
-};
+//var corsOptions = {
+//  origin: "http://localhost:3000",
+//};
 
-app.use(cors(corsOptions));
+//app.use(cors(corsOptions));
 // parse requests of content-type - application/json
-app.use(express.json());
+//app.use(express.json());
 // parse requests of content-type - application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: true }));
+//app.use(express.urlencoded({ extended: true }));
 
 // simple route
 app.get("/", (req, res) => {
